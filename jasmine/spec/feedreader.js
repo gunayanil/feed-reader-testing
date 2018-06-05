37d97343dd8d72c38cdca606a9941641b2c81c1b
gunayanil <gunayanilemre@gmail.com>
2018-06-05 11:20:41 -0700

    added comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,21 @@
+ /* All tests within the $() function to ensure they don't run
+ until the DOM is ready */
  $(function() {
+ 	//RSS Feed Definitions
  	describe('RSS Feeds', function() {
-
+ 		//All feeds are defined and not empty
  		it('are defined', function() {
  			expect(allFeeds).toBeDefined();
  			expect(allFeeds.length).not.toBe(0);
  		});
-
+ 		//URL defined and not empty
  		it('url defined', function() {
  			for (let i = 0; i < allFeeds.length; i++) {
  				expect(allFeeds[i].url).toBeDefined();
  				expect(allFeeds[i].url.length).not.toBe(0);
  			}
  		});
-
+ 		//Name defined and not empty
  		it('name defined', function() {
  			for (let i = 0; i < allFeeds.length; i++) {
  				expect(allFeeds[i].name).toBeDefined();
@@ -21,15 +24,15 @@
  		});
  	});
 
-
+ 	//Menu Features
  	describe('The menu', function() {
  		const hiddenMenu = document.querySelector('.menu-hidden');
  		const hamburgerIcon = document.querySelector('.menu-icon-link');
-
+ 		//Menu element is hidden by default
  		it('should be hidden by default', function() {
  			expect(hiddenMenu.classList.contains('menu-hidden')).toBe(true);
  		});
-
+ 		//Menu visibility changes
  		it('should have it visibility toggled', function() {
  			hamburgerIcon.click();
  			expect(hiddenMenu.classList.contains('menu-hidden')).toBe(false);
@@ -38,21 +41,24 @@
  		});
  	});
 
+ 	//Initial Entries Tests
  	describe('Initial Entries', function() {
  		const entry = document.querySelector('.feed .entry');
-
+ 		//loadFeed() is asynchronous
  		beforeEach(function(done) {
  			loadFeed(0, done);
  		});
-
+ 		//loadFeed() function is called and completes its work
  		it('at least one entry is there(in the feed container)', function() {
  			expect(entry).not.toBe(0);
  		});
 
  	});
 
+ 	//New Feed Selection Tests
  	describe('New Feed Selection', function(){
  		let oldContent, newContent;
+ 		//Get default(0) and new(1) feed
  		beforeEach(function(done) {
  			loadFeed(0, function(){
  				oldContent = document.querySelector('.feed').innerHTML;
@@ -63,7 +69,7 @@
  				});
  			});
  		});
-
+ 		//Check the content changes
  		it('changes the content', function() {
  			expect(newContent).not.toBe(oldContent);
  		});
